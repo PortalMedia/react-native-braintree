f4a8cdddd1758cbe996e8c4e07b44ca5121d802f
Caroline Shea <caroline@portalmedia.com>
2024-02-16 09:40:20 -0500

    remove unused methods in RNBraintree module

--- ios/RNBraintree.m
+++ ios/RNBraintree.m
@@ -2,13 +2,9 @@
 #import "BraintreeCore.h"
 #import "BTCardClient.h"
 #import "BraintreePayPal.h"
-#import "BTDataCollector.h"
-#import "BraintreePaymentFlow.h"
-#import "BraintreeThreeDSecure.h"
 
-@interface RNBraintree() <BTViewControllerPresentingDelegate, BTThreeDSecureRequestDelegate>
+@interface RNBraintree() <BTViewControllerPresentingDelegate>
 @property (nonatomic, strong) BTAPIClient *apiClient;
-@property (nonatomic, strong) BTDataCollector *dataCollector;
 @end
 
 @implementation RNBraintree
@@ -23,7 +19,6 @@ RCT_EXPORT_METHOD(showPayPalModule: (NSDictionary *)options
     NSString *currencyCode = options[@"currencyCode"];
 
     self.apiClient = [[BTAPIClient alloc] initWithAuthorization: clientToken];
-    self.dataCollector = [[BTDataCollector alloc] initWithAPIClient:self.apiClient];
     BTPayPalDriver *payPalDriver = [[BTPayPalDriver alloc] initWithAPIClient: self.apiClient];
 
     BTPayPalCheckoutRequest *request= [[BTPayPalCheckoutRequest alloc] initWithAmount:amount];
@@ -37,185 +32,40 @@ RCT_EXPORT_METHOD(showPayPalModule: (NSDictionary *)options
             reject(@"ONE_TIME_PAYMENT_CANCELLED", @"Payment has been cancelled", nil);
             return;
         }
-        [self.dataCollector collectDeviceData:^(NSString * _Nonnull deviceData) {
-            NSMutableDictionary *billingAddressDictionary = [NSMutableDictionary dictionary];
-            if (tokenizedPayPalAccount.billingAddress) {
-                [billingAddressDictionary addEntriesFromDictionary:@{
-                    @"countryCodeAlpha2": tokenizedPayPalAccount.billingAddress.countryCodeAlpha2 ?: [NSNull null],
-                    @"extendedAddress": tokenizedPayPalAccount.billingAddress.extendedAddress ?: [NSNull null],
-                    @"locality": tokenizedPayPalAccount.billingAddress.locality ?: [NSNull null],
-                    @"postalCode": tokenizedPayPalAccount.billingAddress.postalCode ?: [NSNull null],
-                    @"recipientName": tokenizedPayPalAccount.billingAddress.recipientName ?: [NSNull null],
-                    @"region": tokenizedPayPalAccount.billingAddress.region ?: [NSNull null],
-                    @"streetAddress": tokenizedPayPalAccount.billingAddress.streetAddress ?: [NSNull null],
-                }];
-            }
-            NSMutableDictionary *shippingAddressDictionary = [NSMutableDictionary dictionary];
-            if (tokenizedPayPalAccount.shippingAddress) {
-                [shippingAddressDictionary addEntriesFromDictionary:@{
-                    @"countryCodeAlpha2": tokenizedPayPalAccount.shippingAddress.countryCodeAlpha2 ?: [NSNull null],
-                    @"extendedAddress": tokenizedPayPalAccount.shippingAddress.extendedAddress ?: [NSNull null],
-                    @"locality": tokenizedPayPalAccount.shippingAddress.locality ?: [NSNull null],
-                    @"postalCode": tokenizedPayPalAccount.shippingAddress.postalCode ?: [NSNull null],
-                    @"recipientName": tokenizedPayPalAccount.shippingAddress.recipientName ?: [NSNull null],
-                    @"region": tokenizedPayPalAccount.shippingAddress.region ?: [NSNull null],
-                    @"streetAddress": tokenizedPayPalAccount.shippingAddress.streetAddress ?: [NSNull null],
-                }];
-            }
-            resolve(@{
-                @"nonce": tokenizedPayPalAccount.nonce ?: [NSNull null],
-                @"payerId": tokenizedPayPalAccount.payerID ?: [NSNull null],
-                @"email": tokenizedPayPalAccount.email ?: [NSNull null],
-                @"firstName": tokenizedPayPalAccount.firstName ?: [NSNull null],
-                @"lastName": tokenizedPayPalAccount.lastName ?: [NSNull null],
-                @"phone": tokenizedPayPalAccount.phone ?: [NSNull null],
-                @"billingAddress": billingAddressDictionary,
-                @"shippingAddress": shippingAddressDictionary,
-                @"deviceData": deviceData,
-            });
-        }];
-    }];
-}
-
-RCT_EXPORT_METHOD(requestPayPalBillingAgreement: (NSDictionary *)options
-                  resolver: (RCTPromiseResolveBlock)resolve
-                  rejecter: (RCTPromiseRejectBlock)reject) {
-    NSString *clientToken = options[@"clientToken"];
-    NSString *description = options[@"description"];
-
-    self.apiClient = [[BTAPIClient alloc] initWithAuthorization: clientToken];
-    self.dataCollector = [[BTDataCollector alloc] initWithAPIClient:self.apiClient];
-    BTPayPalDriver *payPalDriver = [[BTPayPalDriver alloc] initWithAPIClient: self.apiClient];
-
-    BTPayPalVaultRequest *request= [[BTPayPalVaultRequest alloc] init];
-    if (description) {
-        request.billingAgreementDescription = description;
-    }
-    [payPalDriver tokenizePayPalAccountWithPayPalRequest:request completion:^(BTPayPalAccountNonce * _Nullable tokenizedPayPalAccount, NSError * _Nullable error) {
-        if (error) {
-            reject(@"REQUEST_BILLING_AGREEMENT_FAILED", error.localizedDescription, nil);
-            return;
+        NSMutableDictionary *billingAddressDictionary = [NSMutableDictionary dictionary];
+        if (tokenizedPayPalAccount.billingAddress) {
+            [billingAddressDictionary addEntriesFromDictionary:@{
+                @"countryCodeAlpha2": tokenizedPayPalAccount.billingAddress.countryCodeAlpha2 ?: [NSNull null],
+                @"extendedAddress": tokenizedPayPalAccount.billingAddress.extendedAddress ?: [NSNull null],
+                @"locality": tokenizedPayPalAccount.billingAddress.locality ?: [NSNull null],
+                @"postalCode": tokenizedPayPalAccount.billingAddress.postalCode ?: [NSNull null],
+                @"recipientName": tokenizedPayPalAccount.billingAddress.recipientName ?: [NSNull null],
+                @"region": tokenizedPayPalAccount.billingAddress.region ?: [NSNull null],
+                @"streetAddress": tokenizedPayPalAccount.billingAddress.streetAddress ?: [NSNull null],
+            }];
         }
-        if (!tokenizedPayPalAccount) {
-            reject(@"REQUEST_BILLING_AGREEMENT_CANCELLED", @"Request billing agreement has been cancelled", nil);
-            return;
+        NSMutableDictionary *shippingAddressDictionary = [NSMutableDictionary dictionary];
+        if (tokenizedPayPalAccount.shippingAddress) {
+            [shippingAddressDictionary addEntriesFromDictionary:@{
+                @"countryCodeAlpha2": tokenizedPayPalAccount.shippingAddress.countryCodeAlpha2 ?: [NSNull null],
+                @"extendedAddress": tokenizedPayPalAccount.shippingAddress.extendedAddress ?: [NSNull null],
+                @"locality": tokenizedPayPalAccount.shippingAddress.locality ?: [NSNull null],
+                @"postalCode": tokenizedPayPalAccount.shippingAddress.postalCode ?: [NSNull null],
+                @"recipientName": tokenizedPayPalAccount.shippingAddress.recipientName ?: [NSNull null],
+                @"region": tokenizedPayPalAccount.shippingAddress.region ?: [NSNull null],
+                @"streetAddress": tokenizedPayPalAccount.shippingAddress.streetAddress ?: [NSNull null],
+            }];
         }
-        [self.dataCollector collectDeviceData:^(NSString * _Nonnull deviceData) {
-            resolve(@{@"deviceData": deviceData,
-                      @"nonce": tokenizedPayPalAccount.nonce,});
-        }];
-    }];
-}
-
-RCT_EXPORT_METHOD(tokenizeCard: (NSDictionary *)parameters
-                  resolver: (RCTPromiseResolveBlock)resolve
-                  rejecter: (RCTPromiseRejectBlock)reject) {
-
-    NSString *clientToken = parameters[@"clientToken"];
-    self.apiClient = [[BTAPIClient alloc] initWithAuthorization: clientToken];
-    self.dataCollector = [[BTDataCollector alloc] initWithAPIClient:self.apiClient];
-    BTCardClient *cardClient = [[BTCardClient alloc] initWithAPIClient: self.apiClient];
-
-    BTCard *card = [[BTCard alloc] init];
-    card.number = parameters[@"number"];
-    card.expirationMonth = parameters[@"expirationMonth"];
-    card.expirationYear = parameters[@"expirationYear"];
-    card.cvv = parameters[@"cvv"];
-    card.postalCode = parameters[@"postalCode"];
-    card.shouldValidate = NO;
-    
-    [cardClient tokenizeCard:card completion:^(BTCardNonce * _Nullable tokenizedCard, NSError * _Nullable error) {
-        if (error) {
-            reject(@"TOKENIZE_FAILED", error.localizedDescription, nil);
-            return;
-        }
-        [self.dataCollector collectDeviceData:^(NSString * _Nonnull deviceData) {
-            resolve(@{@"deviceData": deviceData,
-                      @"nonce": tokenizedCard.nonce,});
-        }];
-    }];
-}
-
-RCT_EXPORT_METHOD(run3DSecureCheck: (NSDictionary *)parameters
-                     resolver: (RCTPromiseResolveBlock)resolve
-                     rejecter: (RCTPromiseRejectBlock)reject) {
-    [self startPaymentFlow:parameters
-                  resolver:resolve
-                  rejecter:reject];
-}
-
-RCT_EXPORT_METHOD(getDeviceData: (NSString *) clientToken
-                  resolver: (RCTPromiseResolveBlock)resolve
-                  rejecter: (RCTPromiseRejectBlock)reject) {
-    self.apiClient = [[BTAPIClient alloc] initWithAuthorization: clientToken];
-    self.dataCollector = [[BTDataCollector alloc] initWithAPIClient:self.apiClient];
-    [self.dataCollector collectDeviceData:^(NSString * _Nonnull deviceData) {
-        resolve(deviceData);
-    }];
-}
-
-#pragma mark - 3D Secure
-- (void)startPaymentFlow: (NSDictionary *)parameters
-                resolver: (RCTPromiseResolveBlock)resolve
-                rejecter: (RCTPromiseRejectBlock)reject {
-    NSString *clientToken = parameters[@"clientToken"];
-    if (self.apiClient == NULL) {
-        if (clientToken == NULL) {
-            reject(@"MISSING_CLIENT_TOKEN", @"clientToken must be passed if Braintree methods weren't run before", nil);
-        }
-        self.apiClient = [[BTAPIClient alloc] initWithAuthorization: clientToken];
-    }
-    self.dataCollector = [[BTDataCollector alloc] initWithAPIClient:self.apiClient];
-
-    BTThreeDSecureRequest *threeDSecureRequest = [[BTThreeDSecureRequest alloc] init];
-    threeDSecureRequest.amount = [NSDecimalNumber decimalNumberWithString: parameters[@"amount"]];
-    threeDSecureRequest.nonce =  parameters[@"nonce"];
-    threeDSecureRequest.threeDSecureRequestDelegate = self;
-    threeDSecureRequest.email = parameters[@"email"];
-    threeDSecureRequest.versionRequested = BTThreeDSecureVersion2;
-
-    BTThreeDSecurePostalAddress *address = [BTThreeDSecurePostalAddress new];
-    address.givenName =  parameters[@"firstname"]; // ASCII-printable characters required, else will throw a validation error
-    address.surname = parameters[@"lastname"]; // ASCII-printable characters required, else will throw a validation error
-    address.phoneNumber = parameters[@"phoneNumber"];
-    address.streetAddress = parameters[@"streetAddress"];
-    address.extendedAddress = parameters[@"streetAddress2"];
-    address.locality = parameters[@"city"];
-    address.region = parameters[@"region"];
-    address.postalCode = parameters[@"postalCode"];
-    address.countryCodeAlpha2 = parameters[@"countryCode"];
-
-    BTThreeDSecureAdditionalInformation *additionalInformation = [BTThreeDSecureAdditionalInformation new];
-    additionalInformation.shippingAddress = address;
-
-    threeDSecureRequest.additionalInformation = additionalInformation;
-    threeDSecureRequest.billingAddress = address;
-
-    BTPaymentFlowDriver *paymentFlowDriver = [[BTPaymentFlowDriver alloc] initWithAPIClient:self.apiClient];
-    paymentFlowDriver.viewControllerPresentingDelegate = self;
-
-    [paymentFlowDriver startPaymentFlow:threeDSecureRequest completion:^(BTPaymentFlowResult * _Nonnull result, NSError * _Nonnull error) {
-        if (error) {
-            reject(@"PAYMENT_FAILED", error.localizedDescription, nil);
-            return;
-        }
-        BTThreeDSecureResult *threeDSecureResult = (BTThreeDSecureResult *)result;
-        if (!threeDSecureResult.tokenizedCard.threeDSecureInfo.liabilityShiftPossible && threeDSecureResult.tokenizedCard.threeDSecureInfo.wasVerified) {
-            reject(@"3DSECURE_NOT_ABLE_TO_SHIFT_LIABILITY", @"3D Secure liability cannot be shifted", nil);
-            return;
-        }
-        if (!threeDSecureResult.tokenizedCard.threeDSecureInfo.liabilityShifted && threeDSecureResult.tokenizedCard.threeDSecureInfo.wasVerified) {
-            reject(@"3DSECURE_LIABILITY_NOT_SHIFTED", @"3D Secure liability was not shifted", nil);
-        }
-        if (!threeDSecureResult.tokenizedCard.nonce) {
-            reject(@"PAYMENT_3D_SECURE_FAILED", @"Something went wrong", nil);
-            return;
-        }
-        [self.dataCollector collectDeviceData:^(NSString * _Nonnull deviceData) {
-            resolve(@{@"deviceData": deviceData,
-                      @"nonce": threeDSecureResult.tokenizedCard.nonce});
-        }];
-        return;
+        resolve(@{
+            @"nonce": tokenizedPayPalAccount.nonce ?: [NSNull null],
+            @"payerId": tokenizedPayPalAccount.payerID ?: [NSNull null],
+            @"email": tokenizedPayPalAccount.email ?: [NSNull null],
+            @"firstName": tokenizedPayPalAccount.firstName ?: [NSNull null],
+            @"lastName": tokenizedPayPalAccount.lastName ?: [NSNull null],
+            @"phone": tokenizedPayPalAccount.phone ?: [NSNull null],
+            @"billingAddress": billingAddressDictionary,
+            @"shippingAddress": shippingAddressDictionary,
+        });
     }];
 }
 
@@ -226,12 +76,6 @@ RCT_EXPORT_METHOD(getDeviceData: (NSString *) clientToken
 
 - (void)paymentDriver:(nonnull id)driver requestsDismissalOfViewController:(nonnull UIViewController *)viewController {
     [viewController dismissViewControllerAnimated:YES completion:nil];
-}
-
-#pragma mark - BTThreeDSecureRequestDelegate
-
-- (void)onLookupComplete:(BTThreeDSecureRequest *)request lookupResult:(BTThreeDSecureResult *)result next:(void (^)(void))next {
-    next();
 }
 
 #pragma mark - RootController
